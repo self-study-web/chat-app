7ca542bdd3e7e0f6f1f42d758b4535a7282bb6df
Bino Varghese <binovarghese85@gmail.com>
2018-07-28 17:59:55 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const express = require("express");
+const publicPath = path.join(__dirname, "../public");
+
+const app = express();
+
+app.use(express.static(publicPath));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at ${PORT}`);
+});
