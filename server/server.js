0a977d9db66c26eb7b0e33669ec8aa7669560c86
Bino Varghese <binovarghese85@gmail.com>
2018-07-29 18:22:03 +0530

    Added styling

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ io.on("connection", socket => {
   socket.on("createMessage", (newMessage, callback) => {
     console.log("createMessage", newMessage);
     io.emit("newMessage", generateMessage(newMessage.from, newMessage.text));
-    callback("This is from server");
+    callback();
   });
 
   socket.on("createLocationMessage", coords => {
